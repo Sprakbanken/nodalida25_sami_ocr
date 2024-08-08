4a66342f3f54c15af4a2dd458e19c0835d577be7
tita <tita.enstad@nb.no>
2024-08-08 12:30:43 +0200

    Oppdater tesseract_transcribe

--- src/tesseract_transcribe.py
+++ src/tesseract_transcribe.py
@@ -3,10 +3,6 @@ from pathlib import Path
 from PIL import Image
 import pytesseract
 from tqdm import tqdm
-import pandas as pd
-from pathlib import Path
-from PIL import Image
-import pytesseract
 import logging
 from utils import setup_logging
 from argparse import ArgumentParser
@@ -17,48 +13,11 @@ pil_logger.setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def transcribe_with_gt(
-    model_name: str,
-    ground_truth_dir: Path,
-    image_file_ext: str = ".tif",
-    text_file_ext: str = ".txt",
-) -> pd.DataFrame:
-    """Run tesseract model on images in ground_truth_dir, and read actual transcriptions from the textfiles in the same directory"""
-    transcriptions = {
-        "model_name": [],
-        "image": [],
-        "transcription": [],
-        "ground_truth": [],
-    }
-
-    img_files = sorted(
-        [e for e in ground_truth_dir.iterdir() if e.suffix == image_file_ext]
-    )
-    txt_files = sorted(
-        [e for e in ground_truth_dir.iterdir() if e.suffix == text_file_ext]
-    )
-
-    for img_file, txt_file in tqdm(zip(img_files, txt_files), total=len(img_files)):
-        transcription = pytesseract.image_to_string(
-            Image.open(img_file), lang=model_name
-        )
-        if not transcription:
-            logger.debug(f"No transcription for {img_file}")
-            logger.debug(transcription)
-        transcriptions["image"].append(img_file.name)
-        transcriptions["model_name"].append(model_name)
-        transcriptions["transcription"].append(transcription)
-        transcriptions["ground_truth"].append(txt_file.read_text())
-
-    df = pd.DataFrame(transcriptions)
-    return df
-
-
 def transcribe(model_name: str, image_dir: Path) -> pd.DataFrame:
     """Run tesseract model on all files in image_dir"""
     transcriptions = {"model_name": [], "image": [], "transcription": []}
-
-    for img in image_dir.iterdir():
+    images = list(image_dir.glob("*.jpg"))
+    for img in tqdm(images):
         transcription = pytesseract.image_to_string(Image.open(img), lang=model_name)
         if not transcription:
             logger.debug(f"No transcription for {img}")
@@ -82,12 +41,10 @@ if __name__ == "__main__":
         type=Path,
         help="The directory containing images to be transcribed",
     )
-    parser.add_argument("output_dir", type=Path, help="The output directory")
     parser.add_argument(
-        "--gt",
-        type=bool,
-        default=True,
-        help="If True, will look for ground truth transcriptions in image dir (if False, assumes image_dir only contains images)",
+        "output_dir",
+        type=Path,
+        help="The output directory to store predicted transcriptions",
     )
     parser.add_argument(
         "--log_level",
@@ -102,21 +59,11 @@ if __name__ == "__main__":
 
     args.output_dir.mkdir(exist_ok=True, parents=True)
 
-    if args.gt:
-        df = transcribe_with_gt(
-            model_name=args.model_name,
-            ground_truth_dir=args.image_dir,
-        )
+    df = transcribe(model_name=args.model_name, image_dir=args.image_dir)
 
-        df.to_csv(
-            args.output_dir
-            / f"{args.image_dir.name}_{args.model_name}_transcriptions_gt.csv",
-            index=False,
-        )
-    else:
-        df = transcribe(model_name=args.model_name, image_dir=args.image_dir)
-        df.to_csv(
-            args.output_dir
-            / f"{args.image_dir.name}_{args.model_name}_transcriptions.csv",
-            index=False,
-        )
+    output_csv = (
+        args.output_dir / f"{args.image_dir.name}_{args.model_name}_transcriptions.csv"
+    )
+
+    df.to_csv(output_csv, index=False)
+    logger.info(f"Wrote transcriptions to {output_csv}")
