4f3f5f5471ff5956162bd299c66415a0763b4ba1
tita <tita.enstad@nb.no>
2024-08-08 10:27:47 +0200

    Lag skript for å inspisere dårlige bokser

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+import pandas as pd
 
 
 def setup_logging(source_script: str, log_level: str):
@@ -12,3 +13,8 @@ def setup_logging(source_script: str, log_level: str):
         level=getattr(logging, log_level),
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
+
+
+def clean_transcriptions(transcriptions: pd.Series) -> pd.Series:
+    """Convert to string and strip"""
+    return transcriptions.apply(str).apply(str.strip)
