24512fe5f39cfe3198a00a5ddb8c73199588a98b
tita <tita.enstad@nb.no>
2024-08-13 11:34:44 +0200

    Fjern og/eller roter linjebilder som er for smale i forhold til høyden

--- src/transkribus_export_to_line_data.py
+++ src/transkribus_export_to_line_data.py
@@ -22,7 +22,11 @@ def get_annotations_for_image(image_path: Path | str) -> list[Annotation]:
     return get_annotation_information(xml_path)
 
 
-def transkribus_export_to_words_pages(base_image_dir: Path, output_dir: Path) -> None:
+def transkribus_export_to_words_pages(
+    base_image_dir: Path,
+    output_dir: Path,
+    img_suffixes: list[str] = [".jpg", ".png", ".tif"],
+) -> None:
     """Adapted from Marie Roalds original code:
     https://github.com/Sprakbanken/hugin-munin-ordbilder/blob/main/scripts/create_unadjusted_dataset.py [Accessed: Aug 7, 2024].
 
@@ -31,7 +35,8 @@ def transkribus_export_to_words_pages(base_image_dir: Path, output_dir: Path) ->
     - Add rows even if image exists
     """
     rows = []
-    for input_image_path in sorted(base_image_dir.glob("**/*.jpg")):
+    images = [e for suf in img_suffixes for e in base_image_dir.glob(f"**/*{suf}")]
+    for input_image_path in sorted(images):
         logger.info(f"Creating output for {input_image_path}")
         annotations = get_annotations_for_image(input_image_path)
 
@@ -57,7 +62,7 @@ def transkribus_export_to_words_pages(base_image_dir: Path, output_dir: Path) ->
             output_page_path = output_page_subdir / input_image_path.name
 
             if output_image_path.exists():
-                logger.info(f"{output_image_path} exists, skipping")
+                logger.debug(f"{output_image_path} exists, skipping")
                 row = {
                     "page_image": str(output_page_path.relative_to(output_dir)),
                     "word_image": str(output_image_path.relative_to(output_dir)),
@@ -104,26 +109,50 @@ def transkribus_export_to_words_pages(base_image_dir: Path, output_dir: Path) ->
 
 
 def words_pages_to_lines(
-    source_data_dir: Path, destination_data_dir: Path, txt_suffix: str
+    source_data_dir: Path,
+    destination_data_dir: Path,
+    txt_suffix: str,
+    rotate: bool,
+    remove: bool,
 ):
     """Create .tif and (.gt).txt line level transcriptions from ordbilder output"""
     metadata_df = pd.read_csv(source_data_dir / "metadata.csv")
 
+    copied = 0
+    rotated = 0
+    removed = 0
+
     for e in metadata_df.itertuples():
         line_image_path = source_data_dir / e.word_image
         assert line_image_path.exists()
-
+        img = Image.open(line_image_path)
+        width, height = img.size
+        if rotate and width * 2 < height:
+            logger.debug(f"Rotating image {e.word_image}")
+            rotated += 1
+            img = img.rotate(-90, expand=True)
+        elif remove and width < height:
+            logger.debug(
+                f"Skipping image {e.word_image} (width to height ratio too small)"
+            )
+            removed += 1
+            continue
         file_stem = line_image_path.stem
         new_image_path = destination_data_dir / f"{file_stem}.tif"
 
-        img = Image.open(line_image_path)
         img.save(new_image_path)
+        copied += 1
 
         # convert numbers to strings and replace non-breaking space character with normal space
         transcription = str(e.word).replace("\xa0", " ")
         text_path = destination_data_dir / f"{file_stem}{txt_suffix}"
         with text_path.open("w+") as f:
             f.write(transcription)
+    logger.info(
+        f"Copied {copied} line images and transcriptions to {destination_data_dir}"
+    )
+    logger.info(f"{rotated} images were rotated due to width/height ratio")
+    logger.info(f"{removed} images were skipped due to width/height ratio")
 
 
 if __name__ == "__main__":
@@ -149,6 +178,16 @@ if __name__ == "__main__":
         help="If flagged, keep <output_dir>/temp with pages, words and metadata.csv",
     )
     parser.add_argument(
+        "--rotate",
+        action="store_true",
+        help="If flagged, rotate images where width is less than half of height",
+    )
+    parser.add_argument(
+        "--remove",
+        action="store_true",
+        help="If flagged, remove files where width is less than height",
+    )
+    parser.add_argument(
         "--gt",
         type=bool,
         default=True,
@@ -170,6 +209,8 @@ if __name__ == "__main__":
         source_data_dir=temp_dir,
         destination_data_dir=args.output_dir,
         txt_suffix=".gt.txt" if args.gt else ".txt",
+        rotate=args.rotate,
+        remove=args.remove,
     )
 
     if not args.keep_temp_dir:
