f5b5f2d8a1ac58963795e4b84fc6a29782973f4a
tita <tita.enstad@nb.no>
2024-08-30 09:16:16 +0200

    Rename tesseract transcribe

--- scripts/tesseract/tesseract_transcribe.sh
+++ scripts/tesseract/tesseract_transcribe.sh
@@ -1,6 +1,6 @@
-pdm run python -m samisk_ocr.tesseract_transcribe fin --line &
-pdm run python -m samisk_ocr.tesseract_transcribe fin_sme --line &
-pdm run python -m samisk_ocr.tesseract_transcribe nor --line &
-pdm run python -m samisk_ocr.tesseract_transcribe nor_sme --line &
-pdm run python -m samisk_ocr.tesseract_transcribe sme --line &
-pdm run python -m samisk_ocr.tesseract_transcribe sme_friis --line &
+pdm run python -m samisk_ocr.tesseract.transcribe fin --line &
+pdm run python -m samisk_ocr.tesseract.transcribe fin_sme --line &
+pdm run python -m samisk_ocr.tesseract.transcribe nor --line &
+pdm run python -m samisk_ocr.tesseract.transcribe nor_sme --line &
+pdm run python -m samisk_ocr.tesseract.transcribe sme --line &
+pdm run python -m samisk_ocr.tesseract.transcribe sme_friis --line &
