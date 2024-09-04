fcc469ae82d3bcf46458e1899ac56675a8763079
tita <tita.enstad@nb.no>
2024-09-04 10:14:06 +0200

    Delvis kopier transkribus exports

---
+++ src/samisk_ocr/add_to_transkribus_export.py
@@ -0,0 +1,44 @@
+import logging
+from argparse import ArgumentParser
+from pathlib import Path
+from shutil import copytree
+
+from samisk_ocr.utils import setup_logging
+
+logger = logging.getLogger(__name__)
+
+
+def has_overlapping_subdirs(input_dir: Path, output_dir: Path) -> bool:
+    existing_files = [e.name for e in output_dir.iterdir()]
+    for sub_dir in input_dir.iterdir():
+        if sub_dir.name in existing_files:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "input_dir",
+        type=Path,
+        help="Path to transkribus export job (directory with directories with alto/, page/ and metadata.xml",
+    )
+    parser.add_argument("output_dir", type=Path, help="Path to larger transkribus export dir")
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        help="If flagged, will overwrite any files that already exist",
+    )
+    args = parser.parse_args()
+    setup_logging(source_script="add_to_transkribus_export", log_level="DEBUG")
+
+    if not args.overwrite and has_overlapping_subdirs(
+        input_dir=args.input_dir, output_dir=args.output_dir
+    ):
+        logger.error(f"Some dirs already exists in {args.output_dir}, but overwrite is not flagged")
+        exit()
+
+    for sub_dir in args.input_dir.iterdir():
+        if not sub_dir.is_dir():
+            continue
+        copytree(sub_dir, dst=args.output_dir / sub_dir.name, dirs_exist_ok=True)
