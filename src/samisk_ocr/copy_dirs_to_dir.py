4803b19eece6ce81d50d57fc5d261086e931b8d3
tita <tita.enstad@nb.no>
2024-09-16 14:17:58 +0200

    Rename generell funksjon til mer generelt navn (ikke spesifikt for transkribus)

--- src/samisk_ocr/copy_dirs_to_dir.py
+++ src/samisk_ocr/copy_dirs_to_dir.py
@@ -21,16 +21,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "input_dir",
         type=Path,
-        help="Path to transkribus export job (directory with directories with alto/, page/ and metadata.xml",
+        help="Path to directory (e.g. transkribus export job directory with subdirectories with alto/, page/ and metadata.xml)",
     )
-    parser.add_argument("output_dir", type=Path, help="Path to larger transkribus export dir")
+    parser.add_argument("output_dir", type=Path, help="Path to directory to copy to")
     parser.add_argument(
         "--overwrite",
         action="store_true",
         help="If flagged, will overwrite any files that already exist",
     )
     args = parser.parse_args()
-    setup_logging(source_script="add_to_transkribus_export", log_level="DEBUG")
+    setup_logging(source_script="copy_dirs_to_dir", log_level="DEBUG")
 
     if not args.overwrite and has_overlapping_subdirs(
         input_dir=args.input_dir, output_dir=args.output_dir
@@ -38,7 +38,5 @@ if __name__ == "__main__":
         logger.error(f"Some dirs already exists in {args.output_dir}, but overwrite is not flagged")
         exit()
 
-    for sub_dir in args.input_dir.iterdir():
-        if not sub_dir.is_dir():
-            continue
+    for sub_dir in args.input_dir.glob("*/"):
         copytree(sub_dir, dst=args.output_dir / sub_dir.name, dirs_exist_ok=True)
