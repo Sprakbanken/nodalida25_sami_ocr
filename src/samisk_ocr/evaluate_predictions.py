79272f8f6d27d3daa91b2f24c1c53dde05704390
tita <tita.enstad@nb.no>
2024-10-02 12:57:43 +0200

    Skriv om transcribe og evaluate_predictions til å transkribere rett inn i metadata-csv

--- src/samisk_ocr/evaluate_predictions.py
+++ src/samisk_ocr/evaluate_predictions.py
@@ -6,9 +6,6 @@ from string import punctuation, whitespace
 
 import pandas as pd
 
-from samisk_ocr.map_transkribus_lines_to_gt_lines import (
-    map_transkribus_image_lines_to_gt_image_lines,
-)
 from samisk_ocr.metrics import SpecialCharacterF1, compute_cer, compute_wer
 from samisk_ocr.utils import setup_logging
 from samisk_ocr.write_characters import get_chars
@@ -19,29 +16,41 @@ logger = logging.getLogger(__name__)
 def get_language_specific_chars(base_model_language: str, gt_chars: str) -> list[str]:
     match base_model_language:
         case "nor":
-            base_language_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÅÆØåæø"
+            base_language_alphabet = (
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÅÆØåæø"
+            )
         case "eng":
-            base_language_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            base_language_alphabet = (
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            )
         case "est":
             base_language_alphabet = (
                 "ABDEFFGHIJKLMNOPRSTUVZZabdeffghijklmnoprstuvzzÄÕÖÜäõöüŠŠššŽŽžž"
             )
         case "fin":
-            base_language_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÅÄÖåäö"
+            base_language_alphabet = (
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÅÄÖåäö"
+            )
         case "":
             logger.info(
                 f"No base model language, using gt_chars - english alphabet to find special characters"
             )
-            base_language_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            base_language_alphabet = (
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            )
         case _:
             logger.warning(
                 f"No alphabet found for base model language {base_model_language}, using gt_chars - english alphabet to find special characters"
             )
-            base_language_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            base_language_alphabet = (
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            )
 
     not_letters = punctuation + whitespace + "«»–§"
     special_letters = [
-        c for c in gt_chars if c not in base_language_alphabet + not_letters and not c.isnumeric()
+        c
+        for c in gt_chars
+        if c not in base_language_alphabet + not_letters and not c.isnumeric()
     ]
     return special_letters
 
@@ -52,15 +61,6 @@ def get_parser() -> ArgumentParser:
         "predictions",
         type=Path,
         help=".csv file with predicted transcriptions",
-    )
-    parser.add_argument(
-        "--dataset", help="Path to dataset", default=Path("data/samisk_ocr_dataset")
-    )
-    parser.add_argument("--split", help="Dataset split to evaluate", default="val")
-    parser.add_argument(
-        "--model_name",
-        help="Name of model that produced transcriptions to evaluate",
-        required=True,
     )
     parser.add_argument(
         "--output_dir",
@@ -79,11 +79,6 @@ def get_parser() -> ArgumentParser:
         help="If flagged, will assume line level predictions (and page level if not flagged)",
     )
     parser.add_argument(
-        "--map_transkribus",
-        action="store_true",
-        help="If flagged, will map transkribus line bboxes to closest ground truth bboxes",
-    )
-    parser.add_argument(
         "--log_level",
         type=str,
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
@@ -100,29 +95,26 @@ if __name__ == "__main__":
 
     df = pd.read_csv(args.predictions)
     df["transcription"] = df.transcription.apply(str)
-
-    if args.map_transkribus:
-        df["image"] = map_transkribus_image_lines_to_gt_image_lines(
-            transkribus_df=df, gt_image_dir=args.gt_transcriptions
-        )
-
-    gt_df = pd.read_csv(args.dataset / args.split / "_metadata.csv")
-    gt_df["image"] = gt_df.file_name.apply(lambda x: Path(x).name)
-    df = df.merge(gt_df, on="image")
     df = df.rename(columns={"text": "ground_truth"})
 
+    model_name = args.predictions.name.rsplit("_", maxsplit=1)[0]
+
     if args.line:
-        output_dir = args.output_dir / "line_level" / args.model_name
+        output_dir = args.output_dir / "line_level" / model_name
     else:
-        output_dir = args.output_dir / "page_level" / args.model_name
+        output_dir = args.output_dir / "page_level" / model_name
     output_dir.mkdir(parents=True)
 
     df["CER"] = df.apply(
-        lambda row: compute_cer(transcription=row.transcription, ground_truth=row.ground_truth),
+        lambda row: compute_cer(
+            transcription=row.transcription, ground_truth=row.ground_truth
+        ),
         axis=1,
     )
     df["WER"] = df.apply(
-        lambda row: compute_wer(transcription=row.transcription, ground_truth=row.ground_truth),
+        lambda row: compute_wer(
+            transcription=row.transcription, ground_truth=row.ground_truth
+        ),
         axis=1,
     )
 
@@ -143,7 +135,9 @@ if __name__ == "__main__":
     )
     general_scorer = SpecialCharacterF1("".join(special_chars))
     df["special_char_F1"] = df.apply(
-        lambda row: general_scorer(transcription=row.transcription, ground_truth=row.ground_truth),
+        lambda row: general_scorer(
+            transcription=row.transcription, ground_truth=row.ground_truth
+        ),
         axis=1,
     )
     collection_level_scores["special_char_F1"] = df.special_char_F1.mean()
--- src/samisk_ocr/tesseract/transcribe.py
+++ src/samisk_ocr/tesseract/transcribe.py
@@ -15,40 +15,45 @@ pil_logger.setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def transcribe(
+def transcribe(model_name: str, image: Path, config: str) -> str:
+    return pytesseract.image_to_string(
+        Image.open(image), lang=model_name, config=config
+    )
+
+
+def transcribe_dataset(
     model_name: str,
-    image_dir: Path,
+    dataset: Path,
+    split: str,
     line_level: bool,
-    img_suffixes: list[str] = [".jpg", ".png", ".tif"],
 ) -> pd.DataFrame:
-    """Run tesseract model on all files in image_dir"""
-    transcriptions = {"model_name": [], "image": [], "transcription": []}
-    images = [e for suf in img_suffixes for e in image_dir.glob(f"*{suf}")]
+    """Run tesseract model on all images in dataset split"""
 
-    tesseract_config = ""
-    if line_level:
-        tesseract_config = "--psm 7"
+    df = pd.read_csv(dataset / split / "_metadata.csv")
+    tesseract_config = "--psm 7" if line_level else ""
 
-    for img in tqdm(images):
-        transcription = pytesseract.image_to_string(
-            Image.open(img), lang=model_name, config=tesseract_config
+    transcriptions = []
+
+    for tup in tqdm(df.itertuples(), total=len(df)):
+        img_file_name = dataset / split / str(tup.file_name)
+        transcription = transcribe(
+            model_name=model_name,
+            image=img_file_name,
+            config=tesseract_config,
         )
         if line_level and not transcription:
-            logger.debug(f"No transcription for {img}")
-            logger.debug("Trying to transcribe with --psm 8 (treat image as single word)")
-            transcription = pytesseract.image_to_string(
-                Image.open(img), lang=model_name, config="--psm 8"
+            logger.debug("No transcription for %s", tup.file_name)
+            logger.debug(
+                "Trying to transcribe with --psm 8 (treat image as single word)"
             )
-
+            transcription = transcribe(
+                model_name, image=img_file_name, config="--psm 8"
+            )
         if not transcription:
-            logger.debug(f"No transcription for {img}")
-            logger.debug(transcription)
+            logger.debug("No transcription for %s", tup.file_name)
+        transcriptions.append(transcription)
 
-        transcriptions["image"].append(img.name)
-        transcriptions["model_name"].append(model_name)
-        transcriptions["transcription"].append(transcription)
-
-    df = pd.DataFrame(transcriptions)
+    df["transcription"] = transcriptions
     return df
 
 
@@ -59,11 +64,12 @@ if __name__ == "__main__":
         help="Name of tesseract model (must be in list of available models with 'tesseract --list-langs')",
     )
     parser.add_argument(
-        "--image_dir",
+        "--dataset",
+        help="Path to local dataset",
         type=Path,
-        help="The directory containing images to be transcribed",
-        default=Path("data/samisk_ocr_dataset/val"),
+        default=Path("data/samisk_ocr_dataset/"),
     )
+    parser.add_argument("--split", default="val", help="Dataset split to transcribe")
     parser.add_argument(
         "--output_dir",
         type=Path,
@@ -71,9 +77,9 @@ if __name__ == "__main__":
         default=Path("output/predictions/"),
     )
     parser.add_argument(
-        "--line",
+        "--page",
         action="store_true",
-        help="If flagged, will treat images as line level",
+        help="If flagged, will treat images as page level",
     )
     parser.add_argument(
         "--log_level",
@@ -86,16 +92,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     setup_logging(source_script="tesseract_transcribe", log_level=args.log_level)
 
-    df = transcribe(model_name=args.model_name, image_dir=args.image_dir, line_level=args.line)
+    df = transcribe_dataset(
+        model_name=args.model_name,
+        dataset=args.dataset,
+        split=args.split,
+        line_level=not args.page,
+    )
 
-    if args.line:
-        output_dir = args.output_dir / "line_level"
-    else:
+    if args.page:
         output_dir = args.output_dir / "page_level"
+    else:
+        output_dir = args.output_dir / "line_level"
 
     output_dir.mkdir(exist_ok=True, parents=True)
-
-    output_csv = output_dir / f"{args.image_dir.name}_{args.model_name}_predictions.csv"
-
+    output_csv = output_dir / f"{args.model_name}_{args.split}.csv"
     df.to_csv(output_csv, index=False)
+
     logger.info(f"Wrote predicted transcriptions to {output_csv}")
