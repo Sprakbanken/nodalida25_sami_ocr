d8f362b83cae992a12c958ffae86bfe5043b7edd
tita <tita.enstad@nb.no>
2024-10-02 10:40:31 +0200

    oppdater scripts

--- scripts/tesseract/eval_validation.sh
+++ scripts/tesseract/eval_validation.sh
@@ -1,6 +1,8 @@
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_nor_smi_nor_predictions.csv --line --model_name nor_smi_nor --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_nor_smi_predictions.csv  --line --model_name nor_smi --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_smi_predictions.csv --line --model_name smi &
 pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_fin_predictions.csv --line --model_name fin --base_model_language fin &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_fin_sme_predictions.csv  --line --model_name fin_sme --base_model_language fin &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_nor_predictions.csv --line --model_name nor --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_fin_sme_predictions.csv --line --model_name fin_sme --base_model_language fin &
 pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_nor_sme_predictions.csv --line --model_name nor_sme --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_sme_friis_predictions.csv --line --model_name sme_friis &
 pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_sme_predictions.csv --line --model_name sme &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/val_sme_friis_predictions.csv --line --model_name sme_friis
--- scripts/tesseract/tesseract_transcribe.sh
+++ scripts/tesseract/tesseract_transcribe.sh
@@ -1,6 +1,3 @@
-pdm run python -m samisk_ocr.tesseract.transcribe fin --line &
-pdm run python -m samisk_ocr.tesseract.transcribe fin_sme --line &
-pdm run python -m samisk_ocr.tesseract.transcribe nor --line &
-pdm run python -m samisk_ocr.tesseract.transcribe nor_sme --line &
-pdm run python -m samisk_ocr.tesseract.transcribe sme --line &
-pdm run python -m samisk_ocr.tesseract.transcribe sme_friis --line &
+pdm run python -m samisk_ocr.tesseract.transcribe smi --line &
+pdm run python -m samisk_ocr.tesseract.transcribe nor_smi --line &
+pdm run python -m samisk_ocr.tesseract.transcribe nor_smi_nor --line
