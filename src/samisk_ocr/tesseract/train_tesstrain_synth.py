5b6f2d6d42c58afaab2960eaacabb4f55d9fc141
tita <tita.enstad@nb.no>
2024-10-04 12:27:11 +0200

    Lag tesseract treningsdata fra syntetisk datasett

--- src/samisk_ocr/tesseract/train_tesstrain_synth.py
+++ src/samisk_ocr/tesseract/train_tesstrain_synth.py
@@ -3,75 +3,34 @@ import logging
 import subprocess
 from argparse import ArgumentParser
 from pathlib import Path
-from shutil import copy2
-from typing import Literal
 
-from datasets import Dataset, load_dataset
+from datasets import load_dataset
 
+from samisk_ocr.tesseract.train_tesstrain import (
+    copy_data,
+    copy_image_files_and_create_gt_files,
+    create_train_eval_lists,
+)
 from samisk_ocr.utils import setup_logging
 
 logger = logging.getLogger(__name__)
 
 
-def copy_image_files_and_create_gt_files(ds: Dataset, output_dir: Path, id_col: str = "urn"):
-    for i, e in enumerate(ds):
-        file_stem = f"{e[id_col]}_{i}"
-        out_p = output_dir / f"{file_stem}.gt.txt"
-        with out_p.open("w+") as f:
-            f.write(e["text"])
-        img = e["image"]
-        img.save(output_dir / f"{file_stem}.png")
-
-
-def create_train_eval_lists(train_ds: Dataset, val_ds: Dataset, output_dir: Path, prefix: str):
-    train_lstmf_names = [f"{prefix}{e['urn']}_{i}.lstmf" for i, e in enumerate(train_ds)]
-    with open(output_dir / "list.train", "w+") as f:
-        f.write("\n".join(train_lstmf_names))
-
-    val_lstmf_names = [f"{prefix}{e['urn']}_{i}.lstmf" for i, e in enumerate(val_ds)]
-    with open(output_dir / "list.eval", "w+") as f:
-        f.write("\n".join(val_lstmf_names))
-
-    with open(output_dir / "all-lstmf", "w+") as f:
-        f.write("\n".join(train_lstmf_names + val_lstmf_names))
-
-
 def create_tesstrain_data(
+    path_to_synthetic_dataset: str,
     path_to_dataset: str,
     model_data_dir: Path,
     model_traindata_dir: Path,
-    filter_wh: bool,
-    filter_len: int,
-    page_30_len: int,
-    page_30: Literal["exclude", "include", "only"],
-    gt_pix: Literal["exclude", "include", "only"],
 ):
-    logger.info("Loading dataset...")
-    dataset = load_dataset("imagefolder", data_dir=path_to_dataset)
-    train_dataset = dataset["train"]
-    val_dataset = dataset["validation"]
+    logger.info("Loading synthetic dataset...")
+    train_dataset = load_dataset("imagefolder", data_dir=path_to_synthetic_dataset, split="train")
 
-    if filter_wh:
-        train_dataset = train_dataset.filter(lambda x: x["width"] > x["height"])
-    if filter_len:
-        train_dataset = train_dataset.filter(lambda x: x["text_len"] > filter_len)
-    if page_30_len:
-        train_dataset = train_dataset.filter(
-            lambda x: not x["page_30"] or x["text_len"] > page_30_len
-        )
-    if page_30 == "exclude":
-        train_dataset = train_dataset.filter(lambda x: not x["page_30"])
-    elif page_30 == "only":
-        train_dataset = train_dataset.filter(lambda x: x["page_30"])
-    if gt_pix == "exclude":
-        train_dataset = train_dataset.filter(lambda x: not x["gt_pix"])
-    elif gt_pix == "only":
-        train_dataset = train_dataset.filter(lambda x: x["gt_pix"])
+    logger.info("Loading validation dataset...")
+    val_dataset = load_dataset("imagefolder", data_dir=path_to_dataset, split="validation")
 
     logger.info(f"Moving train data into {model_traindata_dir}...")
     copy_image_files_and_create_gt_files(
-        train_dataset,
-        output_dir=model_traindata_dir,
+        train_dataset, output_dir=model_traindata_dir, id_col="unique_id"
     )
 
     logger.info(f"Moving validation data into {model_traindata_dir}...")
@@ -92,32 +51,6 @@ def create_tesstrain_data(
     )
 
 
-def copy_data(
-    model_data_dir: Path,
-    model_traindata_dir: Path,
-    other_model_traindata_dir: Path,
-    other_model_data_dir: Path,
-):
-    # Copy training data
-    for e in other_model_traindata_dir.iterdir():
-        copy2(src=e, dst=model_traindata_dir / e.name)
-
-    # Copy lstmf-list files
-    eval_list = other_model_data_dir / "list.eval"
-    train_list = other_model_data_dir / "list.train"
-    all_list = other_model_data_dir / "all-lstmf"
-
-    def replace_data_dir_filename(lstmf_filename: str) -> str:
-        data_dir, other_model_traindata_dir_name, filename = lstmf_filename.split("/")
-        return "/".join((data_dir, model_traindata_dir.name, filename))
-
-    for file_ in (eval_list, train_list, all_list):
-        filenames = file_.read_text().split("\n")
-        new_filenames = [replace_data_dir_filename(e) for e in filenames]
-        with open(model_data_dir / file_.name, "w+") as f:
-            f.write("\n".join(new_filenames))
-
-
 def get_parser() -> ArgumentParser:
     parser = ArgumentParser(
         description="Create training data and training script to train tesseract model"
@@ -125,37 +58,11 @@ def get_parser() -> ArgumentParser:
     parser.add_argument("model_name", help="Name of tesseract model")
     parser.add_argument("--tessdata", help="Path to tessdata (see tesseract_howto)", required=True)
     parser.add_argument("--tesstrain_repo", help="Path to tesstrain repo", default="tesstrain")
+    parser.add_argument(
+        "--synth_dataset_path", help="Path to synthetic dataset", default="data/syntetisk_dataset"
+    )
     parser.add_argument("--dataset_path", help="Path to dataset", default="data/samisk_ocr_dataset")
-    parser.add_argument(
-        "--filter_wh",
-        action="store_true",
-        help="If flagged, will filter out images where width < height",
-    )
-    parser.add_argument(
-        "--filter_len",
-        type=int,
-        metavar="n",
-        help="If provided, will filter out images where transcription is shorter than n",
-    )
-    parser.add_argument(
-        "--page_30",
-        choices=["only", "include", "exclude"],
-        default="exclude",
-        help="Whether to include, exclude or transfer only the page_30 data",
-    )
-    parser.add_argument(
-        "--filter_len_30",
-        type=int,
-        metavar="n",
-        help="If provided, will filter out page_30 images where transcription is shorter than n",
-    )
 
-    parser.add_argument(
-        "--gt_pix",
-        choices=["only", "include", "exclude"],
-        default="exclude",
-        help="Whether to include, exclude or transfer only the gt_pix data",
-    )
     parser.add_argument("--num_epochs", type=int, default=5)
     parser.add_argument("--learning_rate", type=float, default=0.002)
     parser.add_argument(
@@ -210,14 +117,10 @@ if __name__ == "__main__":
         model_traindata_dir.mkdir()
         model_data_dir.mkdir()
         create_tesstrain_data(
+            path_to_synthetic_dataset=args.synth_dataset_path,
             path_to_dataset=args.dataset_path,
             model_data_dir=model_data_dir,
             model_traindata_dir=model_traindata_dir,
-            filter_len=args.filter_len,
-            filter_wh=args.filter_wh,
-            page_30=args.page_30,
-            page_30_len=args.filter_len_30,
-            gt_pix=args.gt_pix,
         )
 
     tesstrain_arg_list = [
