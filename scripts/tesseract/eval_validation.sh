776e27f250cdbbbad85c5f47640370f1d43fd350
tita <tita.enstad@nb.no>
2024-10-17 14:04:56 +0200

    Revert "Merge branch 'tesseract_trening' into transkribus_sammenlikn"
    This reverts commit f5fa99bef02767fd986c8e5a8e71edf117226747, reversing
    changes made to 1c34721db6668c591079ec87e474d173dc9f17e5.

--- scripts/tesseract/eval_validation.sh
+++ scripts/tesseract/eval_validation.sh
@@ -1,17 +1,15 @@
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_fin_sme_val.csv --base_model_language fin &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_fin_val.csv --base_model_language fin &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_nor_sme_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_nor_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_sme_friis_val.csv &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_sme_val.csv &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_smi_nor_pred_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_smi_nor_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_smi_pred_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_smi_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tess_ub_smi_val.csv &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transk_smi_nor_pred_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transk_smi_nor_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transk_smi_val.csv --base_model_language nor &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transk_ub_smi_val.csv &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/trocr_smi_nor_val.csv --base_model_language eng &
-pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/trocr_smi_val.csv --base_model_language eng &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/fin_sme_val.csv --base_model_language fin &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/fin_val.csv --base_model_language fin &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/nor_sme_val.csv --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/nor_smi_nor_val.csv --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/nor_smi_val.csv --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/nor_val.csv --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/sme_friis_val.csv &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/sme_val.csv &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/smi_val.csv &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tr_ocr_nor_smi_val.csv --base_model_language eng &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/tr_ocr_smi_val.csv --base_model_language eng &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transkribus_med_alt_val.csv --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transkribus_med_base_norsk_val.csv --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transkribus_med_base_val.csv --base_model_language nor &
+pdm run python -m samisk_ocr.evaluate_predictions output/predictions/line_level/transkribus_uten_base_val.csv --base_model_language nor &
--- scripts/tesseract/tesseract_transcribe.sh
+++ scripts/tesseract/tesseract_transcribe.sh
@@ -1,5 +1,9 @@
+pdm run python -m samisk_ocr.tesseract.transcribe fin &
+pdm run python -m samisk_ocr.tesseract.transcribe fin_sme &
+pdm run python -m samisk_ocr.tesseract.transcribe nor &
+pdm run python -m samisk_ocr.tesseract.transcribe nor_sme &
+pdm run python -m samisk_ocr.tesseract.transcribe sme &
+pdm run python -m samisk_ocr.tesseract.transcribe sme_friis &
+pdm run python -m samisk_ocr.tesseract.transcribe nor_smi &
 pdm run python -m samisk_ocr.tesseract.transcribe smi &
-pdm run python -m samisk_ocr.tesseract.transcribe smi_nor &
-pdm run python -m samisk_ocr.tesseract.transcribe smi_nor_pred &
-pdm run python -m samisk_ocr.tesseract.transcribe smi_pred &
-pdm run python -m samisk_ocr.tesseract.transcribe ub_smi &
+pdm run python -m samisk_ocr.tesseract.transcribe nor_smi_nor
